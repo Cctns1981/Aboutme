3e566606df4418e7a90f95cee35cfa673e951d42
C.J. Scott <132962397+Cctns1981@users.noreply.github.com>
2023-06-03 18:52:27 -0500

    add documents

---
+++ aboutme.js
@@ -0,0 +1,65 @@
+'use striect'
+let person = prompt('Do I like Tulsa Oklahoma?');
+person = person.toUpperCase() 
+// console.log(person)
+if (person === "NO") {
+//console.log ("No. I do not");
+alert('You are wrong.');
+} else if (person === 'YES' || person === 'Y'){
+  //console.log ("You're Right");
+alert("You're Right");
+}
+//alert (person);
+// let color = promp()
+let color = prompt('Do I like the color Red?');
+color = color.toUpperCase() 
+// console.log(color)
+if (color === "NO") {
+//console.log ("No. I do not");
+alert('You are wrong');
+} else if (color === 'YES' || color === 'Y'){
+  //console.log ("You're Right");
+alert("You're Right");
+}
+//alert (person);
+// let color = promp()
+let education = prompt('Did I graduate from Middle Tennesee State University?');
+education = education.toUpperCase() 
+// console.log(education)
+if (education === "NO") {
+//console.log ("No. I do not");
+alert('You are wrong');
+} else if (education === 'YES' || education === 'Y'){
+  //console.log ("You're Right");
+alert("You're Right");
+}
+//alert (person);
+// let color = promp()
+let peace = prompt('Do I like Nashville?');
+peace = peace.toUpperCase() 
+// console.log(peace)
+if (peace === "NO") {
+//console.log ("No. I do not");
+alert('You are wrong');
+} else if (peace === 'YES' || peace === 'Y'){
+  //console.log ("You're Right");
+alert("You're Right");
+}
+//alert (person);
+// let color = promp()
+let people = prompt('Do I like Animals?');
+people = people.toUpperCase() 
+// console.log(person)
+if (people === "NO") {
+//console.log ("No. I do not");
+alert('You are wrong');
+} else if (people === 'YES' || people === 'Y'){
+  //console.log ("You're Right");
+alert("You're Right");
+}
+//alert (person);
+// let color = promp()
+
+let username = prompt ('What is your Name?');
+// console.log(username);
+alert('Greetings ' + username)
