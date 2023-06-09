9ce3b4d6d109776083b9898ea31f100bf47c34c5
C.J. Scott <132962397+Cctns1981@users.noreply.github.com>
2023-06-08 22:53:02 -0500

    updated java

--- aboutme.js
+++ aboutme.js
@@ -1,4 +1,5 @@
-'use striect'
+'use strict';
+let userPoints =0
 let person = prompt('Do I like Tulsa Oklahoma?');
 person = person.toUpperCase() 
 // console.log(person)
@@ -57,9 +58,70 @@ alert('You are wrong');
   //console.log ("You're Right");
 alert("You're Right");
 }
+let number = '';
+let i = 0;
+while(number !== "5"){
+  //console/log(first game :', i)
+number = prompt('Guess a number between 1 and 10');
+if(number < 3){
+  alert('too low');
+}else if (number > 5){
+  alert('too high');
+}
+i++;
+if( i === 5){
+  number = '5';
+  alert('Your Right')
+  break;
+}
+}
+i++;
+if(i<4){
+  number = "5"
+}
+for(let i = 0; i < 4; i++){
+  console.log('second game : ', i)
+  let number2 = prompt( 'guess a number between 1 and 10');
+  if(number2 === "5"){
+    alert('correct');
+    break;
+  }
+}
+if(i === 2){
+  alert('out of guesses')
+}
+
+let food = String['Chicken Wings', 'Pizza', 'Tacos', 'French Fries', 'Hamburgers', 'Nachos', 'Ribs'];
+console.log(food);
+let correct = false;
+
+for (let j = 0; j < 7; j++) {
+  console.log(food);
+  let answer = prompt ('Guess one of my Favorite Foods').toLocaleLowerCase();
+
+for (let k = 0; k < food.length; k++) {
+    if (answer === food[k]) {
+      userPoints++;
+      console.log('Yes ${answer} is one of my favorite foods.');
+      alert('Yes ${answer} is one of my favortite foods. You get a point.');
+      correct = true;
+      break;
+    } 
+  }
+    if (correct === true) {
+      break;
+  } else if (j < 6) {
+      console.log('That is not on my List, Please try again.');
+      alert('That is not on my List, Please try again.');
+  } else {
+      console.log ('Sorry, you have run out of Guesses.');
+      alert('Sorry, you have run out of guesses.');
+    }
+  }
 //alert (person);
 // let color = promp()
 
 let username = prompt ('What is your Name?');
 // console.log(username);
 alert('Greetings ' + username)
+//console.log(username);
